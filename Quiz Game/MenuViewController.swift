9ec76389bba441622e4996504b7758bdacf7b61e
harryhan24 <herryhan2435@gmail.com>
2017-07-09 16:08:19 +0900

    first init

--- Quiz Game/MenuViewController.swift
+++ Quiz Game/MenuViewController.swift
@@ -7,6 +7,11 @@
 //
 
 import UIKit
+import AVFoundation
+
+var backgroundMusicPlayer: AVAudioPlayer = AVAudioPlayer()
+var wrongMusicPlayer: AVAudioPlayer = AVAudioPlayer()
+var correctMusicPlayer: AVAudioPlayer = AVAudioPlayer()
 
 class MenuViewController: UIViewController {
     
@@ -41,6 +46,20 @@ class MenuViewController: UIViewController {
         navigationController?.navigationBar.tintColor = UIColor.white
         view.backgroundColor = UIColor(red: 41/255, green: 128/255, blue: 185/255, alpha: 1.0)
         layoutView()
+        
+        let backgroundURL = Bundle.main.url(forResource: "background", withExtension: "wav")
+        backgroundMusicPlayer = try! AVAudioPlayer(contentsOf: backgroundURL!)
+        backgroundMusicPlayer.numberOfLoops = -1
+        backgroundMusicPlayer.prepareToPlay()
+        backgroundMusicPlayer.play()
+        
+        let wrongURL = Bundle.main.url(forResource: "wrong", withExtension: "aiff")
+        wrongMusicPlayer = try! AVAudioPlayer(contentsOf: wrongURL!)
+        wrongMusicPlayer.prepareToPlay()
+        
+        let correctURL = Bundle.main.url(forResource: "correct", withExtension: "mp3")
+        correctMusicPlayer = try! AVAudioPlayer(contentsOf: correctURL!)
+        correctMusicPlayer.prepareToPlay()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -176,7 +195,7 @@ class MenuViewController: UIViewController {
         case 1:
             vc = ImageQuizViewController()
         case 2:
-            print("Right Wrong")
+            vc = RightWrongQuizViewController()
         case 3:
             vc = EmojiQuizViewController()
         default:
